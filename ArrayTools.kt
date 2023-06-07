cefd4a36646472fa08d79783e1cec85e5e7cc2b4
Dennis Padilla <d_padilla.y@hotmail.com>
2023-06-07 11:49:46 -0400

    fixed bugs in func#2 added more testing

--- ArrayTools.kt
+++ ArrayTools.kt
@@ -1,73 +1,88 @@
-fun main() {}
+fun main() {
 
-    //Encrpt function #1
-    fun encrypt(inputString: String, shiftAmount: Int): String {
-        val array1 = inputString.toCharArray()
+    //Test Function #2
+    averageArrayTester ();
 
-        // Modifying the original array
-        for (i in 0 until shiftAmount) {
-            val temp = array1[array1.size - 1] // Store the last element temporarily
+}
 
-            for (j in array1.size - 1 downTo 1) {
-                array1[j] = array1[j - 1] // Shift each element to the right
-            }
+//Encrpt function #1
+fun encrypt(inputString: String, shiftAmount: Int): String {
+    val array1 = inputString.toCharArray()
 
-            array1[0] = temp // Place the stored element at the beginning
+    // Modifying the original array
+    for (i in 0 until shiftAmount) {
+        val temp = array1[array1.size - 1] // Store the last element temporarily
+
+        for (j in array1.size - 1 downTo 1) {
+            array1[j] = array1[j - 1] // Shift each element to the right
         }
-        val shiftedString = array1.joinToString("") //array1 es converted back to a string
-        return shiftedString //returns shifted string
+
+        array1[0] = temp // Place the stored element at the beginning
     }
-    // Prompt the user for input
-    print("Enter a string: ")
-    val inputString = readLine() ?: "" // Read user input, default to empty string if null
-    print("Enter a shift amount: ")
-    val shiftAmount = readLine()?.toIntOrNull() ?: 0 // Read user input as Int, default to 0 if null or invalid
-    
-    val encryptedString: String = encrypt(inputString, shiftAmount) // Store the result in a val
-    println("Encrypted String: $encryptedString") // Print the encrypted string
+    val shiftedString = array1.joinToString("") //array1 es converted back to a string
+    return shiftedString //returns shifted string
+}
+// Prompt the user for input
+print("Enter a string: ")
+val inputString = readLine() ?: "" // Read user input, default to empty string if null
+print("Enter a shift amount: ")
+val shiftAmount = readLine()?.toIntOrNull() ?: 0 // Read user input as Int, default to 0 if null or invalid
+
+val encryptedString: String = encrypt(inputString, shiftAmount) // Store the result in a val
+println("Encrypted String: $encryptedString") // Print the encrypted string
 
 /**
 *Averages the elements of an Integer Array and outputs the average as a double
 *@param inputArray The  array that will be averaged
-*@return The average value of inputArray
+*@return Double - Average values as Double, or 0 if inputArray is empty
 */
-    fun arrayAvg(inputArray: Array<Int>): Double {
-        var total = 0.0 // initialize total as double
-        for (element in inputArray) {
-            total += element // Accumulate the sum ef elements in the array
-        }
-        return (total / inputArray.size) //Calculate the average divding the total by the array size
+fun arrayAvg(inputArray: Array<Int>): Double {
+    if(inputArray.isEmpty()){ //Avoid divide by 0 in return
+        return 0.0
     }
-    
-    println("The average of the array is ${arrayAvg(arrayOf(1, 2, 3, 4, 5, 6))}") //print result
-    
+    var total = 0.0 // initialize total as double
+    for (element in inputArray) {
+        total += element // Accumulate the sum ef elements in the array
+    }
+    return (total / inputArray.size) //Calculate the average divding the total by the array size
+}
+
+fun averageArrayTester (){
+    println("The average of the array is ${arrayAvg(arrayOf(1, 2, 3))} " ) //Expect 2
+    println("The average of the array is ${arrayAvg(arrayOf(-1, 2, -3))} " ) //Expect: -2/3
+    println("The average of the array is ${arrayAvg(arrayOf(-1909))} " ) //Expect -1909
+    println("The average of the array is ${arrayAvg(arrayOf(-0))} " ) //Expect: 0
+    println("The average of the array is ${arrayAvg(arrayOf())} " ) //Expect: 0
+}
 
 
-    //Array Contain Function #3
-    fun arrayContains (inputArray: Array<Int>, searchValue: Int): Boolean {
-        for (element in inputArray) {
-            if (element == searchValue) {
-                return true
-        
-            }
+
+//Array Contain Function #3
+fun arrayContains (inputArray: Array<Int>, searchValue: Int): Boolean {
+    for (element in inputArray) {
+        if (element == searchValue) {
+            return true
+    
         }
-        return false
+    }
+    return false
+}
+
+
+println(arrayContains(arrayOf(1,2,3,4,5,6),0)) //print result
+
+//Reverse Array function #4
+fun reverseArray(inputArray: Array<Int>): Array<Int> {
+    val reversedArray = Array(inputArray.size) { 0 } // Create a new array of the same size
+
+    for (i in 0 until inputArray.size) {
+        reversedArray[i] = inputArray[inputArray.size - 1 - i] // Copy elements in reverse order
     }
 
-    println(arrayContains(arrayOf(1,2,3,4,5,6),0))
+    return reversedArray
+}
 
-    //Reverse Array function #4
-    fun reverseArray(inputArray: Array<Int>): Array<Int> {
-        val reversedArray = Array(inputArray.size) { 0 } // Create a new array of the same size
-    
-        for (i in 0 until inputArray.size) {
-            reversedArray[i] = inputArray[inputArray.size - 1 - i] // Copy elements in reverse order
-        }
-    
-        return reversedArray
-    }
-    
-    val originalArray = arrayOf(1, 2, 3, 4, 5, 6)
-    val reversedArray = reverseArray(originalArray)
-    println("Reversed Array: ${reversedArray.joinToString(", ")}")
+val originalArray = arrayOf(1, 2, 3, 4, 5, 6)
+val reversedArray = reverseArray(originalArray)
+println("Reversed Array: ${reversedArray.joinToString(", ")}")
 
